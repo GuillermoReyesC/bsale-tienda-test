d589631553076c03f49e5cc3b86a4bd439a4b9d8
Guillermo Reyes Campos <guillermo.a.reyes.c@gmail.com>
2022-01-16 00:43:37 -0300

    documentation added

--- app.js
+++ app.js
@@ -1,20 +1,60 @@
 
+/** 
+ * estas son las url de los datos que se van a traer 
+ * esta utilizado con una instancia de CorsAnywhere
+ *  Que permite hacer peticiones a una url desde cualquier lugar
+ * @type {object}
+ * 
+ * 
+ * 
+ *  **/
 
-// url del API
 const urlProducts = 'https://aqueous-fjord-68634.herokuapp.com/https://thawing-eyrie-55353.herokuapp.com/products/';
 
 const urlCategories = 'https://aqueous-fjord-68634.herokuapp.com/https://thawing-eyrie-55353.herokuapp.com/categories/'
 
 
+/**
+ * registramos el request del api
+ * y se activa DOMContentLoaded cuando el html esta listo
+ *
+ *  **/
+
 document.addEventListener("DOMContentLoaded", () => {
     fetch(urlProducts)
 })
+document.addEventListener("DOMContentLoaded", () => {
+    fetch(urlCategories)
+})
        
+/**
+ * con las funciones fetch traemos los datos de la api
+ * y con el metodo json() los convertimos a json
+ * los guardamos en una variable
+ * e imprimimos un error en consola si no hay datos
+ *
+ *  **/
+
 fetch(urlProducts)
     .then(response => response.json())
     .then(data => mostrarData(data))
     .catch(error => console.log(error))
-    
+
+fetch(urlCategories)
+    .then(response => response.json())
+    .then(dataCateg => console.log(dataCateg)) // categoria => terminar
+    .catch(error => console.log(error))
+     
+
+/**
+ *  los datos guardados los pasamos por las funciones
+ *  respectivas ( pintarProductos, detectarBotones)
+ *  para que se dibujen en el html
+ * @type {json} data
+ * 
+ * 
+ *  
+ *  **/
         
 const mostrarData = (data) => {
     pintarProductos(data)
@@ -23,6 +63,20 @@ const mostrarData = (data) => {
 }
 
 const contendorProductos = document.querySelector('#contenedor-productos')
+/**
+ * 
+ * @param { object } carrito datos del carrito
+ * @param { number } carrito.id  identificador del carrito
+ * @param { number } carrito.cantidad cantidad de productos en el carrito
+ * @param { number } carrito.price precio del producto
+ * @type {element} contenedorProductos
+ * 
+ *   
+ * 
+ *  **/
+
+
+
 function pintarProductos  (data)  {
     const template = document.querySelector('#template-productos').content
     const fragment = document.createDocumentFragment()
@@ -41,7 +95,23 @@ function pintarProductos  (data)  {
     contendorProductos.appendChild(fragment)
 }
 
+/**
+ * @param {object} carrito datos del carrito
+ * 
+ * declaramos la constante carrito vacia para que no se repita
+ * 
+ *  **/
+
+
 let carrito = {}
+
+/**
+ *  detectamos la funcion click en los botones
+ * y agregamos el producto a pintar carrito, en el html para
+ * que se vea en el carrito de compras
+ * 
+ *  
+ *  **/
 
 const detectarBotones = data => {
     const botones = document.querySelectorAll('.card button')
@@ -61,9 +131,30 @@ const detectarBotones = data => {
     })
 }
 
+/**
+ *  
+ * 
+ * 
+ * 
+ *  **/
 
 const items = document.querySelector('#items')
 
+
+/**
+ * la funcion pintarCarrito recibe la data e imprime 
+ * en el html elementos del carrito
+ * y los botones de accion.
+ * 
+ * @param { object } carrito datos del carrito
+ * @param { number } carrito.id  identificador del carrito
+ * @param { number } carrito.cantidad cantidad de productos en el carrito
+ * @param { number } carrito.price precio del producto
+ * @type {element} contenedorProductos
+ * 
+ * 
+ * 
+ *  **/
 const pintarCarrito = () => {
 
     //pendiente innerHTML
@@ -93,7 +184,12 @@ const pintarCarrito = () => {
     })
 
     items.appendChild(fragment)
-
+/**
+ * funciones que procesan informacion (data) para imprimir el footer
+ * y detectar los botones de accion, respectivamente.
+ * 
+ * 
+ *  **/
     pintarFooter()
     accionBotones()
 
@@ -104,17 +200,38 @@ const pintarFooter = () => {
 
     footer.innerHTML = ''
 
+/**
+ * si el carrito esta vacio, se imprime un <th> con "carrito vacio"
+ * obviamente, si no, se imprime un <th> con el total de productos
+ * y un <th> con el total de precio.    
+ * 
+ * 
+ * 
+ *  **/
+
+
+
     if (Object.keys(carrito).length === 0) {
         footer.innerHTML = `
-        <th scope="row" colspan="5">Carrito vacío con innerHTML</th>
+        <th scope="row" colspan="5">Carrito vacío!</th>
         `
         return
     }
 
+
+ 
+
     const template = document.querySelector('#template-footer').content
     const fragment = document.createDocumentFragment()
 
-    // sumar cantidad y sumar totales
+    
+    /**
+     * se declaran las constantes para el total de productos y precio
+     *  
+     * 
+     *  **/
+
+
     const nCantidad = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0)
     const nPrecio = Object.values(carrito).reduce((acc, {cantidad, price}) => acc + cantidad * price ,0)
      //console.log(carrito)
@@ -141,11 +258,28 @@ const pintarFooter = () => {
 
 }
 
+/**
+ * 
+ *  * agrega los botones agregar y eliminar producto de la lista
+ * de productos en el carrito
+ * 
+ *  **/
+
 const accionBotones = () => {
     const botonesAgregar = document.querySelectorAll('#items .btn-info')
     const botonesEliminar = document.querySelectorAll('#items .btn-danger')
 
     // console.log(botonesAgregar)
+
+/**
+ * por cada boton agregar, se le agrega un evento click
+ * y se le agrega una funcion que se ejecuta al hacer click
+ * que agrega productos al carrito
+ * @param {producto} producto
+ * 
+ * 
+ *  **/
+
 
     botonesAgregar.forEach(btn => {
         btn.addEventListener('click', () => {
@@ -172,7 +306,7 @@ const accionBotones = () => {
     })
 }
 
-//busqeuda de items por id  
+ 
 
 const searchResult = document.querySelector('#search-result');
 const searchInput = document.querySelector('#search-input');
@@ -180,24 +314,33 @@ const botonSearch = document.querySelector('#buscar-items');
 
 
 
+
 botonSearch.addEventListener('click', filter);
+
+
+/**
+ * la funcion searchItem trae con fetch los items de la url concatenando el nombre del item 
+ * o los items a buscar con la variable input, que recoge los datos del search-input
+ * y para mostrarlos con searchResult.
+ * 
+ *  
+ * @param {items} items - lista de items a filtrar
+ * @param {searchInput} searchInput - input de busqueda
+ * @param {searchResult} searchResult - resultado de la busqueda
+ * 
+ * 
+ * **/
 
 function searchItems  () {
     
-
-
   const input = document.getElementById("search-input").value;
   fetch('https://aqueous-fjord-68634.herokuapp.com/https://thawing-eyrie-55353.herokuapp.com/products/'+ input )
   .then(response => response.json())
   .then(data => pintarProductos(data));
-    
 
 
 }
 
-function pay () {
-    alert('pagaste XD')
-}
 
  
 
---
+++ index.js
@@ -0,0 +1,10 @@
+/** 
+ * @file index.html the main file of the application
+ * @author Guillermo Reyes
+ * @see <a href="https://github.com/GuillermoReyesC/bsale-tienda-test" target="_blank">Github</a>
+ * @version 1.0.0
+ * @text ojala les sirva <3
+ * 
+ * 
+ * 
+*/
